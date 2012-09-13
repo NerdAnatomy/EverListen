e3dbb2214f23558d4b74e705585773233c8f5681
NerdAnatomy <jungseob.shin@outlook.com>
2012-09-13 09:38:25 +0900

    2012. 9. 13
    1. First project created
    2. Hello, EverNote!

---
+++ EverListen/src/com/example/everlisten/EverListenActivity.java
@@ -0,0 +1,24 @@
+package com.example.everlisten;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class EverListenActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ever_listen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_ever_listen, menu);
+        return true;
+    }
+
+    
+}
